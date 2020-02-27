2d4eb3a2c705124ccc4248fe305ff59fb296f747
Matt Hummel <marketing@users.noreply.github.com>
2020-02-27 12:31:07 -0500

    Logo
    Update url

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
       
       <div class="container">
         <a class="navbar-brand" href="#">
-          <img src="http://78bbm3rv7ks4b6i8j3cuklc1-wpengine.netdna-ssl.com/wp-content/uploads/germanna-logo-white.svg" alt="germanna white logo" height="60" width="200">
+          <img src="https://78bbm3rv7ks4b6i8j3cuklc1-wpengine.netdna-ssl.com/wp-content/uploads/germanna-logo-white.svg" alt="germanna white logo" height="60" width="200">
         </a>
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
