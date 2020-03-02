78673ad5e3eee098e6331023d69c9da8bf8afed5
Matt Hummel <marketing@users.noreply.github.com>
2020-03-02 16:06:06 -0500

    Hero
    Fix video script

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,24 +1,6 @@
 $( document ).ready(function() {
 
-$(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-        
-        $('#back-to-top').tooltip('show');
+$('video').playbackRate = 0.52;
 
 });
 
-document.querySelector('video').playbackRate = 0.52;
--- assets/js/main.js
+++ assets/js/main.js
@@ -15,27 +15,9 @@
  //# sourceMappingURL=bootstrap.min.js.map
 $( document ).ready(function() {
 
-$(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-        
-        $('#back-to-top').tooltip('show');
+$('video').playbackRate = 0.52;
 
 });
 
-document.querySelector('video').playbackRate = 0.52;
 
 //# sourceMappingURL=main.js.map
