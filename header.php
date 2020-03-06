405d58b295c04d620ea51572ca05a9c35559df66
Matt Hummel <marketing@users.noreply.github.com>
2020-03-06 10:28:04 -0500

    Header
    Add ga tracking code

--- header.php
+++ header.php
@@ -16,6 +16,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-11698249-9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-11698249-9');
+</script>
 </head>
 
 <body <?php body_class(); ?>>
